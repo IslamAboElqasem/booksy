e60de3e6eeabc87d31849394835ad0cdc25e2347
islam <heshamislam556@gmail.com>
2024-02-18 20:11:01 +0200

    "add get x "

--- book_app/lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ book_app/lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -58,7 +58,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHome() {
     Future.delayed(
-      const Duration(seconds: 2),
+      const Duration(milliseconds: 250),
       () {
         Get.to(() => const HomeView(),
             transition: Transition.fade, duration: kTransitionDuration);
--- book_app/lib/main.dart
+++ book_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:book_app/constant.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'Features/splash/presentation/views/splash_view.dart';
 
@@ -14,7 +14,7 @@ class BookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: primaryColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kprimaryColor),
       home: const SplashView(),
     );
   }
