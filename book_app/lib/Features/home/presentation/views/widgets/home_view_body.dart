2a90e0211f70c4ae3a58d6fceaf0219cf682993a
islam <heshamislam556@gmail.com>
2024-02-19 23:01:32 +0200

    create text best seller and its items creare all styles for app

--- book_app/lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ book_app/lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:book_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:book_app/core/utilits/Styles.dart';
+import 'package:book_app/core/utilits/assets.dart';
 import 'package:flutter/material.dart';
 
 import 'books_listview_horizontal.dart';
@@ -9,22 +10,52 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-        BookListViewHorizontal(),
-        SizedBox(
-          height: 50,
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Text(
-            'Best Seller',
-            style: Styles.titleMedium,
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          BookListViewHorizontal(),
+          SizedBox(
+            height: 50,
           ),
-        ),
-      ],
+          Text(
+            'Best Seller',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          BestSellerViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerViewItem extends StatelessWidget {
+  const BestSellerViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+              aspectRatio: 2.5 / 4.1,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.red,
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(AssetsData.listOfBooks[5])),
+                ),
+              )),
+        ],
+      ),
     );
   }
 }
