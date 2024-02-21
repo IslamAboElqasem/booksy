7c1be01e89f4fa044659487f18f86b69cf04f7f1
islam <heshamislam556@gmail.com>
2024-02-21 02:26:08 +0200

    create custom app bar in book detail view

--- book_app/lib/Features/home/presentation/views/widgets/best_seller_view_item.dart
+++ book_app/lib/Features/home/presentation/views/widgets/best_seller_view_item.dart
@@ -1,8 +1,10 @@
 import 'package:book_app/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:book_app/constant.dart';
+import 'package:book_app/core/utilits/app_routes.dart';
 import 'package:book_app/core/utilits/assets.dart';
 import 'package:book_app/core/utilits/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerViewItem extends StatelessWidget {
   const BestSellerViewItem({super.key, required this.kItem});
@@ -11,64 +13,71 @@ class BestSellerViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-              aspectRatio: 2.5 / 4.1,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.red,
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage(
-                          AssetsData.mapOfBestSeller.keys.toList()[kItem])),
-                ),
-              )),
-          const SizedBox(
-            width: 30,
-          ),
-          Expanded(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SizedBox(
-                height: 55,
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: Text(
-                  AssetsData.mapOfBestSeller.values.toList()[kItem][0],
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Text(
-                AssetsData.mapOfBestSeller.values.toList()[kItem][1],
-                style: Styles.textStyle14,
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Row(
-                children: [
-                  Text(
-                    AssetsData.mapOfBestSeller.values.toList()[kItem][2],
-                    style: Styles.textStyle20
-                        .copyWith(fontWeight: FontWeight.w900),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+      },
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+                aspectRatio: 2.5 / 4.1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.red,
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage(
+                            AssetsData.mapOfBestSeller.keys.toList()[kItem])),
                   ),
-                  const Spacer(),
-                  BookRating(
-                    kItem: kItem,
-                  )
-                ],
-              )
-            ]),
-          )
-        ],
+                )),
+            const SizedBox(
+              width: 30,
+            ),
+            Expanded(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 55,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Text(
+                        AssetsData.mapOfBestSeller.values.toList()[kItem][0],
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Styles.textStyle20
+                            .copyWith(fontFamily: kGtSectraFine),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Text(
+                      AssetsData.mapOfBestSeller.values.toList()[kItem][1],
+                      style: Styles.textStyle14,
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          AssetsData.mapOfBestSeller.values.toList()[kItem][2],
+                          style: Styles.textStyle20
+                              .copyWith(fontWeight: FontWeight.w900),
+                        ),
+                        const Spacer(),
+                        BookRating(
+                          kItem: kItem,
+                        )
+                      ],
+                    )
+                  ]),
+            )
+          ],
+        ),
       ),
     );
   }
