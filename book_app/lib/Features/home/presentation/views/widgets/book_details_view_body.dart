cf15e1e837f914432f20bee2393ca1b0ac748c8b
islam <heshamislam556@gmail.com>
2024-02-21 03:25:17 +0200

    add title and subtitle to details

--- book_app/lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ book_app/lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:book_app/Features/home/presentation/views/widgets/custom_appbar_book_details.dart';
+import 'package:book_app/core/utilits/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'cistom_list_view_item.dart';
@@ -15,11 +16,27 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomAppBarBookDetails(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.15),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.16),
             child: const CustomListViewItem(
               kItyem: 6,
             ),
           ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            textAlign: TextAlign.center,
+            'Harry Potter and the Philosopher’s Stone',
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Text(
+            'J.k. Rowling',
+            style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+          )
         ],
       ),
     );
