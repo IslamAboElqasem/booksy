5135768f3b358a1ca092789754a7a343d3c9b677
islam <heshamislam556@gmail.com>
2024-02-26 23:57:15 +0200

    create rebo methods and add dartz package and create failure file

--- book_app/lib/Features/home/data/repos/home_repo.dart
+++ book_app/lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:book_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeatureBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks();
 }
