29bbd2b7780d47897148b60d3e4fa04ab3e5923f
islam <heshamislam556@gmail.com>
2024-02-27 02:53:32 +0200

    ceate server failure from dio exception and implement home repo

--- book_app/lib/Features/home/data/repos/home_repo_impl.dart
+++ book_app/lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,18 +1,50 @@
 import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:book_app/Features/home/data/repos/home_repo.dart';
 import 'package:book_app/core/errors/failures.dart';
+import 'package:book_app/core/utilits/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?q=subject:Programming&Filtering=free-ebook&Sorting=newest');
+
+      List<BookModel> books = [];
+      for (var item in data['item']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?q=subject:Programming&Filtering=free-ebook');
+
+      List<BookModel> books = [];
+      for (var item in data['item']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
--- book_app/lib/core/errors/failures.dart
+++ book_app/lib/core/errors/failures.dart
@@ -1,3 +1,47 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String msgError;
+
+  const Failure(this.msgError);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.msgError);
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Recieve tmeout with ApiServer');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponce(
+            dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was canceled');
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected error, please try again later');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate');
+      case DioExceptionType.connectionError:
+        return ServerFailure('No Internet Connection');
+    }
+  }
+  factory ServerFailure.fromResponce(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('your request not found, Please try again');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internet Server error, Please try again larer');
+    } else {
+      return ServerFailure('Opps there was an Error, Please try again');
+    }
+  }
+}
