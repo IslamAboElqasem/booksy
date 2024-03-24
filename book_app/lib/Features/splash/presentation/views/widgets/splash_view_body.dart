5aa5b00960076f75e93fe74202f03c621feab6f4
islam <heshamislam556@gmail.com>
2024-03-24 06:49:02 +0200

    Update navigation to pushReplacement in SplashViewBody widget

--- book_app/lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ book_app/lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -57,7 +57,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
     Future.delayed(
       const Duration(seconds: 2),
       () {
-        GoRouter.of(context).push('/homeView');
+        GoRouter.of(context).pushReplacement('/homeView');
       },
     );
   }
